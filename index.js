4b3840ace222779f34cf2d9037cb5d36a99a1e14
Jen Weber <j@jenweber.me>
2020-10-29 22:20:14 -0400

    includes the date in the csv

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
 10. Don't use regex (unless you need it)
 11. Document inputs and outputs
 12. Commit every time something new works
+13. Write code that catches errors
 */
 
 /*
@@ -22,7 +23,7 @@
 ../rfcs/text/
 3. Write the list of files to the CSV
 4. Write the RFC number and the filename to the CSV
-5. Get the Start Date, RFC PR 
+5. Get the Start Date, RFC PR url
 
 
 The output we want in the end is:
@@ -32,7 +33,8 @@ RFC number, RFC file name, PR link, Start Date
 */
 
 const fs = require('fs');
-const fse = require('fs-extra')
+const fse = require('fs-extra');
+const path = require('path');
 
 const outputFile = './output.csv'
 const rfcsPath = '../rfcs/text/'
@@ -41,6 +43,19 @@ const getRfcNumber = (filename) => {
     // example input 0408-decorators.md
     // desired output is 0408
     return filename.substring(0, 4)
+}
+
+const readDate = (filename) => {
+    // example input 0408-decorators.md
+    // output 2014-08-18 
+
+    let fileContents = fs.readFileSync(path.join('../rfcs/text', filename), 'utf8')
+    let startDate = fileContents.match(/\d\d\d\d\-\d\d\-\d\d/)
+
+    if (!startDate) {
+        console.warn('No start date found in ' + filename)
+    }
+    return fileContents.match(/\d\d\d\d\-\d\d\-\d\d/)
 }
 
 
@@ -53,7 +68,8 @@ const results = fileList
         // input is 0408-decorators.md
         // output is 0408, 0408-decorators.md
         let rfcNumber = getRfcNumber(file)
-        return `${rfcNumber}, ${file}`
+        let startDate = readDate(file)
+        return `${rfcNumber}, ${file}, ${startDate}`
     })
     .join('\n')
 
